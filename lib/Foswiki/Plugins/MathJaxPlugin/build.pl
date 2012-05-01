8468640f70cb7175b63642da9b768b6bf09aa378
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/MathJaxPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/MathJaxPlugin/build.pl
+++ lib/Foswiki/Plugins/MathJaxPlugin/build.pl
@@ -6,17 +6,17 @@ use Foswiki::Contrib::Build;
 our @ISA = qw( Foswiki::Contrib::Build );
 
 use FindBin;
+
 #use File::Find;
 
 sub new {
     my $class = shift;
-    my $this = bless( $class->SUPER::new( "MathJaxPlugin" ), $class );
+    my $this = bless( $class->SUPER::new("MathJaxPlugin"), $class );
 
     $this->{DEBUG} = 1;
 
     return $this;
 }
-
 
 sub target_build {
     my $this = shift;
@@ -28,30 +28,34 @@ sub target_build {
     chdir $base_dir or die $!;
 
     my $mathjax = 'MathJax-v1.1a.zip';
-    my $file = $base_dir . $mathjax;
+    my $file    = $base_dir . $mathjax;
     $this->{DEBUG} && print STDERR "file=[$file]\n";
 
     unless ( -e $file ) {
-        system( wget => '-O'=>$mathjax, 'https://github.com/mathjax/MathJax/zipball/v1.1a');
+        system(
+            wget => '-O' => $mathjax,
+            'https://github.com/mathjax/MathJax/zipball/v1.1a'
+        );
     }
 
     system( unzip => '-u' => $mathjax );
-    rename('mathjax-MathJax-f5cd294', 'MathJax');
+    rename( 'mathjax-MathJax-f5cd294', 'MathJax' );
 }
 
 ################################################################################
 
 package main;
+
 # Create the build object
 my $build = new BuildBuild();
 
-$build->{UPLOADTARGETWEB} = 'Extensions';
-$build->{UPLOADTARGETPUB} = 'http://foswiki.org/pub';
+$build->{UPLOADTARGETWEB}    = 'Extensions';
+$build->{UPLOADTARGETPUB}    = 'http://foswiki.org/pub';
 $build->{UPLOADTARGETSCRIPT} = 'http://foswiki.org/bin';
 $build->{UPLOADTARGETSUFFIX} = '';
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
 ################################################################################
 ################################################################################
